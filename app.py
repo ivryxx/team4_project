054c0aab7bd7838f0fbd717e1b63c1ee44c862df
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-08 16:46:19 +0900

    로그인 화면 배경 수정 및 크롤링 세팅

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
-from pymongo import MongoClient
-import jwt
 import datetime
 import hashlib
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
+
+import jwt
+import requests
+
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
+
+#크롤링 세팅
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+weather_data = requests.get('https://weather.com/ko-KR/weather/monthly/l/250893e65830bc39c66ad7582ad9a9e42c04ecac094d360be7eb4e12240b92ef/', headers=headers)
+movie_data = requests.get('https://movie.naver.com/movie/sdb/browsing/bmovie_genre.naver', headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+weather = BeautifulSoup(weather_data.text, 'html.parser')
+movie = BeautifulSoup(weather_data.text, 'html.parser')
+
+print(weather, movie)
 
 #몽고디비연결
 from pymongo import MongoClient
