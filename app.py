c19a06afd1c1005cdba2048fcc6a75e10919a7bb
jasonpizzaboy <80494361+jasonpizzaboy@users.noreply.github.com>
2022-03-09 14:54:29 +0900

    영화 데이터 크롤링 완료

--- app.py
+++ app.py
@@ -14,79 +14,54 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-# 크롤링 세팅
-
-# 타겟 URL을 읽어서 HTML를 받아오고,
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-movie_jenre_data = requests.get('https://movie.naver.com/movie/running/current.naver', headers=headers)
-
-# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
-# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
-# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
-soup = BeautifulSoup(movie_jenre_data.text, 'html.parser')
-
-# content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dt > a
-# content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(2) > dl > dt > a
-
-# content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > div > a > img
-# content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(2) > div > a > img
-
-# content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd.star > dl.info_star > dd > div > a > span.num
-# content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(2) > dl > dd.star > dl.info_star > dd > div > a > span.num
-
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt > a:nth-child(1)
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt > a:nth-child(2)
-
-#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li:nth-child(1) > dl > dd.info_t1 > div > a
-
-movieNames = soup.select('#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li')
-movieImages = soup.select('#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li')
-movieScores = soup.select('#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li')
-movieJenres = soup.select('#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li')
-
-for movieNm in movieNames:
-    a = movieNm.select_one('dl > dt > a')
-    if a is not None:
-        movieName = a.text
-        #print(movieName)
-
-for movieImg in movieImages:
-    a = movieImg.select_one('div > a > img')
-    if a is not None:
-        movieImage = a['src']
-        print(movieImage)
-
-# @app.route('/movie/<movieImg>')
-# def show_movie(movieImg):
-#     status_recieve = request.args.get("status_give","old")
-#     r = requests.get(f"https://movie.naver.com/movie/running/current.naver/{movieImg}", headers={"Authorization": "Token[토큰]"})
-#     if r.status_code !=200:
-#         return redirect(url_for("index", msg = "이미지를 불러오지 못했습니다."))
-#     result = r.json()
-#     return render_template("index.html", movieImg=movieImage)
-
-for movieScr in movieScores:
-    a = movieScr.select_one('dl > dd.star > dl.info_star > dd > div > a > span.num')
-    if a is not None:
-        movieScore = a.text
-        #print(movieScore)
-
-for movieJnr in movieJenres:
-    a = movieJnr.select_one('dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt > a:nth-child(1)')
-    if a is not None:
-        movieJenre = a.text
-        #print(movieJenre)
-
 # 몽고디비연결
+# 크롤링 세팅
 from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
-
 client = MongoClient('mongodb+srv://test:sparta@cluster0.1jgyj.mongodb.net/Cluster0?retryWrites=true&w=majority',
                      tlsCAFile=ca)
 db = client.sparta
+
+# 타겟 URL을 읽어서 HTML를 받아오고,
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+movie_data = requests.get('https://movie.naver.com/movie/running/current.naver', headers=headers)
+soup = BeautifulSoup(movie_data.text, 'html.parser')
+
+# 공통 코드 추출
+movies = soup.select('#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li')
+
+# 반복문 돌면서 아래 코드 실행
+for movie in movies:
+    title = movie.select_one('dl > dt > a').text
+    if title is not None:
+
+        movieName = title
+        movieImage = movie.select_one('div > a > img')['src']
+        movieScore = movie.select_one('dl > dd.star > dl.info_star > dd > div > a > span.num').text
+        movieJenre = movie.select_one('dl > dd:nth-child(3) > dl > dd:nth-child(2) > span.link_txt > a:nth-child(1)').text
+        print(movieName, movieImage, movieScore, movieJenre)
+        doc = {
+            'movieNm': movieName,
+            'movieImg': movieImage,
+            'movieScr': movieScore,
+            'movieJnr': movieJenre
+        }
+
+        db.movieData.insert_one(doc)
+
+
+@app.route("/movieData", methods=["GET"])
+def movie_listing():
+    movie_list = list(db.movieData.find({}, {'_id':False}))
+    return jsonify({'movies': movie_list})
+
 
 
 @app.route('/')
